814edb49ba9d04317efc5a26ccc90b72648fc69e
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-07 00:40:33 -0600

    initial commit of index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<DOCTYPE HTML>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8" />
+		</head>
+		<body>
+			<p>A basic webpage to be manipulated by javascript.  More to be added later.</p>
+		</body>
+	</html>
