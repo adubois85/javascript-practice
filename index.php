4917d1ae6f3f346c25c20b050f574a9facadd175
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-07 00:50:29 -0600

    added css/scripts dirs, relevant files, corrected doctype declare

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<DOCTYPE HTML>
+<!DOCTYPE HTML>
 	<html lang="en">
 		<head>
 			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<!-- A custom CSS stylesheet-->
+			<link rel="stylesheet" href="css/style.css" type="text/css" />
+			<!--A custom Javascript file-->
+			<script type="text/javascript" src="scripts/script.js"></script>
+			<title>Some Fun With Javascript!</title>
 		</head>
 		<body>
 			<p>A basic webpage to be manipulated by javascript.  More to be added later.</p>
