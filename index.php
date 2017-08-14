b38a078f7deb0ac628fb12d3047c347cc98d4ff7
Alexander DuBois <adubois@alumni.uci.edu>
2017-08-14 03:55:32 -0600

    moved body into divs and containers for bootstrap

--- index.php
+++ index.php
@@ -20,32 +20,51 @@
 		<title>Some Fun With Javascript!</title>
 	</head>
 	<body>
-			<h1>Test header to see if I configured my Windows computer correctly.</h1>
-			<h1>A basic webpage to be manipulated by javascript.  More to be added later.</h1>
-			<p id="color-change" onmouseenter="fontChangeColorMouseOver()" onmouseleave="fontChangeColorMouseOut()">
-				The color of this text will change when moused over! Every day is taco ipsum tuesday. Tacos al pastor made with adobada meat. Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. CARNE ASADA!! Tacos for breakfast, lunch and dinner. Does guac cost extra? TACOS!! Tacos al pastor made with adobada meat. Josh’s taco shack is the best taco shack. Yeah, apparently the taco shack was robbed. They left the money but took the tacos.
-			</p>
-			<p id="font-change" ondblclick="fontChangeFamilyDoubleClick()">
-				The font face of this text will change when double-clicked! Burritos are very tasty. Give me all the tacos, immediately. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Does guac cost extra? Tacos, again? This will be 5 times this week and it’s only Tuesday. How do you feel about hard shelled tacos? TACOS!! Let’s do a beef and a chicken, and one with both. It’s taco Tuesday Monday. Give me all your tacos.
-			</p><br>
-			<img id="image-change" src="images/starry-night.jpg" onclick="imageChange()" alt="The Starry Night" />
-			<p>This image will change back and forth between two images when clicked!</p>
-
-			<div>
-				<p>This button will find all instances of the word "taco" and replace it with kitten.</p>
-				<button id="kitty-button" onclick="kittyButton()">Kitty!</button>
-			</div>
-
-			<div>
-				<label>Enter your heart's desire and submit<input type="text" id="input-field" placeholder="I desire..."></label>
-				<button id="button" onclick="submitButton()">Submit</button>
-			</div>
-			<div class="bacon">
-				<button id="bacon-button">Mmm, bacon</button>
-				<div id="bacon-container">
+		<section>
+			<div class="jumbotron">
+				<div class="container">
+					<h1>Test header to see if I configured my Windows computer correctly.</h1>
+					<p>A basic webpage to be manipulated by javascript.  More to be added later.</p>
 				</div>
 			</div>
-
+		</section>
+		<section>
+			<div class="container">
+				<p id="color-change" onmouseenter="fontChangeColorMouseOver()" onmouseleave="fontChangeColorMouseOut()">
+					The color of this text will change when moused over! Every day is taco ipsum tuesday. Tacos al pastor made with adobada meat. Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins. CARNE ASADA!! Tacos for breakfast, lunch and dinner. Does guac cost extra? TACOS!! Tacos al pastor made with adobada meat. Josh’s taco shack is the best taco shack. Yeah, apparently the taco shack was robbed. They left the money but took the tacos.
+				</p>
+				<p id="font-change" ondblclick="fontChangeFamilyDoubleClick()">
+					The font face of this text will change when double-clicked! Burritos are very tasty. Give me all the tacos, immediately. It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. Does guac cost extra? Tacos, again? This will be 5 times this week and it’s only Tuesday. How do you feel about hard shelled tacos? TACOS!! Let’s do a beef and a chicken, and one with both. It’s taco Tuesday Monday. Give me all your tacos.
+				</p>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<img id="image-change" src="images/starry-night.jpg" onclick="imageChange()" alt="The Starry Night" />
+				<p>This image will change back and forth between two images when clicked!</p>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="kitty-button-container">
+					<p>This button will find all instances of the word "taco" and replace it with kitten.</p>
+					<button id="kitty-button" onclick="kittyButton()">Kitty!</button>
+				</div>
+				<div class="desire-input-container">
+					<label>Enter your heart's desire and submit<input type="text" id="input-field" placeholder="I desire..."></label>
+					<button id="button" onclick="submitButton()">Submit</button>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="bacon">
+					<button id="bacon-button">Mmm, bacon</button>
+					<div id="bacon-container">
+					</div>
+				</div>
+			</div>
+		</section>
 		<!-- linking the jQuery library-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
